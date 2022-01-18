ab54cf45672e766cedf1d2aa90862a8694a63bfc
mastiaha <mastiaha@mail.ru>
2022-01-18 17:50:56 +0300

    Добавьте файлы проекта.

---
+++ 21 Laba/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _21_Laba
+{
+    class Program
+    {
+        const int n = 10;
+        static int[] path = new int[n] { 1, 2, 0, 5, 5, 0, 1, 2, 6, 10 };
+        static void Main(string[] args)
+        {
+            ThreadStart threadStart = new ThreadStart(Gardner1);
+            Thread thread = new Thread(threadStart);
+            thread.Start();
+
+            Gardner2();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{path[i]} ");
+            }
+            Console.ReadKey();
+        }
+
+        static void Gardner1()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (path[i] >= 0)
+                {
+                    int delay = path[i];
+                    path[i] = -1;
+                    Thread.Sleep(delay);
+                }
+            }
+        }
+
+
+        static void Gardner2()
+        {
+            for (int i = n - 1; i >= 0; i--)
+            {
+                if (path[i] >= 0)
+                {
+                    int delay = path[i];
+                    path[i] = -2;
+                    Thread.Sleep(delay);
+                }
+            }
+        }
+    }
+}
---
+++ Эадача 21.1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Эадача_21._1
+{
+    class Program
+    {
+         static int[,] uchastok;
+         static int m;
+         static int n;
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк двумерного массива");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов двумерного массива");
+            m = Convert.ToInt32(Console.ReadLine());
+
+            uchastok = new int[n, m];
+
+            Thread sadovnik1 = new Thread(Sadovnik1);
+            Thread sadovnik2 = new Thread(Sadovnik2);
+
+            sadovnik1.Start();
+            sadovnik2.Start();
+
+            sadovnik1.Join();
+            sadovnik2.Join();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(uchastok[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Sadovnik1()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (uchastok[i, j] == 0)
+                        uchastok[i, j] = 1;
+                    Thread.Sleep(1);
+                }
+            }
+        }
+
+        private static void Sadovnik2()
+        {
+            for (int i = m - 1; i > 0; i--)
+            {
+                for (int j = n - 1; j > 0; j--)
+                {
+                    if (uchastok[j, i] == 0)
+                        uchastok[j, i] = 2;
+                    Thread.Sleep(1);
+                }
+            }
+        }
+        
+    }
+}
